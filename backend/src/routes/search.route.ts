0e9588712f8553f5b0aeeba06dd2ce8a693ae9b9
Raid Benakcha <kichoukh00@gmail.com>
2023-04-09 12:49:22 +0200

    ajout de calcul de temps

--- backend/src/routes/search.route.ts
+++ backend/src/routes/search.route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { advancedSearch, simpleSearch } from "../services/search.service";
+import { performance } from "perf_hooks";
 
 const router = Router();
 
@@ -8,10 +9,20 @@ router.get("/", async (req, res) => {
     const query = req.query.query;
     const type = req.query.type;
     if (type === "simple") {
+      const startTime = performance.now();
       const documentIds = await simpleSearch(query as string);
+      const endTime = performance.now();
+      console.log(
+        "simple search for " + query + " took " + (endTime - startTime) + "ms"
+      );
       res.send(documentIds);
     } else {
+      const startTime = performance.now();
       const books = await advancedSearch(query as string);
+      const endTime = performance.now();
+      console.log(
+        "simple search for " + query + " took " + (endTime - startTime) + "ms"
+      );
       res.send(books);
     }
   } catch (err) {
